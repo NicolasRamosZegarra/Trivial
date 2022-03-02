83d28fc71f01649c0c7dda8df1bdf5de4f74a03f
Nicolas <nicolaszegarra90@gmail.com>
2022-03-02 21:05:41 +0100

    eliminamos codigo repetido de posicion de jugador

--- test/TrivialTests.java
+++ test/TrivialTests.java
@@ -3,6 +3,27 @@ import org.junit.jupiter.api.Test;
 import smellytrivial.Game;
 
 public class TrivialTests {
+
+    @Test
+    public void si_al_principio_saco_un_1_voy_a_casilla_1(){
+        //Arrange
+        Game sut = new Game();
+        sut.agregar("Maria");
+        sut.agregar("Juan");
+
+        sut.tirarDado(1);
+
+        String expected = "La nueva posicion de Maria es 1";
+
+        //Act
+        String actual = sut.nuevaPosicionJugador();
+
+        //Assert
+        Assertions.assertEquals(expected,actual);
+    }
+
+
+
     @Test
     public void true_is_true(){
         Assertions.assertTrue(false);
